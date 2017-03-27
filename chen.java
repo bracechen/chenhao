82c5369585f3671dd3e1130cf0e1623a805e732e
bracechen <c0514214@gmail.com>
2017-03-27 17:01:13 +0800

    Create chen.java

---
+++ chen.java
@@ -0,0 +1,11 @@
+public class test {
+        public startic void main(String[] args){
+                int A=45;
+                int B=78;
+                int t=A;
+                A=B;
+                B=t;
+               System.out.println("A的值为"+A);
+               System.out.println("B的值为"+B);
+           }
+}
